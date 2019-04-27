c3d4c213dceaa47edf6f8a37687566d482513eef
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 20:42:26 +0800

    setkan pembolehubah untuk masuk pangkalan data

--- app/core/Constant.php
+++ app/core/Constant.php
@@ -2,3 +2,33 @@
 
 # Sentiasa menyediakan garis condong di belakang (/) pada hujung jalan
 define('BASEURL', dirname('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']) . '/');
+############################################################################################
+## isytihar konsan MYSQL dan GAMBAR ikut lokasi $server
+$ip = $_SERVER['REMOTE_ADDR'];
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$server = $_SERVER['SERVER_NAME'];
+
+/*
+echo "<br>Alamat IP : <font color='red'>" . $ip . "</font> |
+\r<br>Nama PC : <font color='red'>" . $hostname . "</font> |
+\r<br>Server : <font color='red'>" . $server . "</font>\r";
+//*/
+
+if ($server == 'laman.web.anda')
+{	# isytihar tatarajah mysql
+	define('DB_TYPE', 'mysql');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', '***');
+	define('DB_USER', '***');
+	define('DB_PASS', '***');
+}
+else
+{	# isytihar tatarajah mysql
+	define('DB_TYPE', 'mysql');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', '***');
+	define('DB_USER', '***');
+	define('DB_PASS', '***');
+}
+//echo DB_HOST . "," . DB_USER . "," . DB_PASS . ",," . DB_NAME . "<br>";
+############################################################################################
