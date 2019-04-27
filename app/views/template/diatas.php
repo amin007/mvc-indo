6d4f7b40b870e753fdc19ac2ad34ed3c32f214fc
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 16:59:18 +0800

    tambah template diatas dan dibawah

---
+++ app/views/template/diatas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Bootstrap & Font Awesome</title>
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" rel="stylesheet" type="text/css">
+</head>
+<body>
---
+++ app/views/template/dibawah.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
