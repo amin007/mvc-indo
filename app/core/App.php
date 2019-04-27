946afa3316b1322864ad7d1640b9c078ea45770d
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 14:10:48 +0800

    tambah koding dalam parseURL

--- app/core/App.php
+++ app/core/App.php
@@ -8,13 +8,17 @@ class App
 	{
 		$url = $this->parseURL();
 		echo '<pre>semak $url=>';var_dump($url);echo '</pre>';
+		echo '<pre>semak $_GET[url]=>';print_r($_GET['url']);echo '</pre>';
+		echo '<pre>semak $url=>';print_r($url);echo '</pre>';
 	}
 #------------------------------------------------------------------------------------------
 	public function parseURL()
 	{
 		if( isset($_GET['url']) )
 		{
-			$url = $_GET['url'];
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
 			return $url;
 		}
 	}
