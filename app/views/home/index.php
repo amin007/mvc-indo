6624b7ae78d6029a3c26cad0a96912077f85f3d4
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 16:33:01 +0800

    susun balik koding

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,14 +1,17 @@
 <?php
 #-----------------------------------------------------------------------------------------------------------
-function ditengah01($namafail)
-{
-	diatas();
+//$fail = basename($_SERVER['PHP_SELF']);//Returns The Current PHP File Name
+$fail = 'Index';
+$folder = basename(__DIR__);//Returns The Current PHP Folder
+#-----------------------------------------------------------------------------------------------------------
+diatas();
 print <<<END
 <br><br>
 <div class="container mt-10">
 <div class="jumbotron jumbotron-fluid">
 	<div class="container">
-		<h1 class="display-4">Views: Ini Folder <strong>$namafail</strong></h1>
+		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
+		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
 		<p class="lead">Sini adalah kawasan larangan merokok.</p>
 		<a class="btn btn-primary btn-lg" href="../index.php" role="button">Kembali</a>
 	</div>
@@ -16,9 +19,4 @@ print <<<END
 </div><!-- class="container" -->
 END;
 	dibawah();
-}
-#-----------------------------------------------------------------------------------------------------------
-//$namafail = basename($_SERVER['PHP_SELF']);//Returns The Current PHP File Name
-$namafail = basename(__DIR__);//Returns The Current PHP Folder
-ditengah01($namafail);
 #-----------------------------------------------------------------------------------------------------------
