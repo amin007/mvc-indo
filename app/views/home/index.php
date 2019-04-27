472b1b25ae081c1388387f2b319c0c3d7237547f
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 18:36:16 +0800

    tukar pautan

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,14 +5,13 @@ $fail = 'Index';
 $folder = basename(__DIR__);//Returns The Current PHP Folder
 #-----------------------------------------------------------------------------------------------------------
 print <<<END
-<br><br>
-<div class="container mt-10">
+<div class="container mt-5">
 <div class="jumbotron jumbotron-fluid">
 	<div class="container">
 		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
 		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
 		<p class="lead">Sini adalah kawasan larangan merokok.</p>
-		<a class="btn btn-primary btn-lg" href="../index.php" role="button">Kembali</a>
+		<a class="btn btn-primary btn-lg" href="#" role="button">Kembali</a>
 	</div>
 </div>
 </div><!-- class="container" -->
