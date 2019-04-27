da929752fafc1d4d91f1a272b2fd484a92f8fb8d
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 13:30:59 +0800

    membentuk koding baru dan memanggil fail init.php

--- public/index.php
+++ public/index.php
@@ -50,8 +50,10 @@ print <<<END
 END;
 }
 #-----------------------------------------------------------------------------------------------------------
-
 //$namafail = basename($_SERVER['PHP_SELF']);//Returns The Current PHP File Name
-$namafail = basename(__DIR__);//Returns The Current PHP Folder
-ditengah($namafail);
-
+//$namafail = basename(__DIR__);//Returns The Current PHP Folder
+//ditengah($namafail);
+#-----------------------------------------------------------------------------------------------------------
+# mula koding asas
+require_once '../app/init.php';
+$app = new App;
