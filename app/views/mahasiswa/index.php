6c6eb3f815610ea1bceb461b592fcd2237df10f2
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-28 00:18:58 +0800

    tambah borang tambah data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -9,6 +9,11 @@ $folder = basename(__DIR__);//Returns The Current PHP Folder
 	<div class="row">
 		<div class="col-6">
 			<h3><?php echo $data['tajukAtas'] ?></h3>
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-toggle="modal"
+			data-target="#formModal">
+			Tambah Data Mahasiswa
+			</button>
 
 			<?php if( isset($data['mhs']) ):?>
 			<ul class="list-group">
@@ -25,3 +30,66 @@ $folder = basename(__DIR__);//Returns The Current PHP Folder
 		</div><!-- / class="col-6" -->
 	</div><!-- / class="row" -->
 </div><!-- class="container" -->
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog"
+aria-labelledby="judulModal" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+<?php $post = BASEURL . 'mahasiswa/tambah'; ?>
+		<form method="POST" action="<?php echo $post ?>" autocomplete="off">
+			<div class="modal-header">
+				<h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="form-group row">
+	<label for="kodNama" class="col-sm-2 col-form-label">Nama</label>
+	<div class="col-sm-10"><div class="input-group mb-2">
+		<div class="input-group-prepend"><div class="input-group-text">@</div></div>
+		<input type="text" name="nama" class="form-control" id="kodInputNama" placeholder="nama">
+	</div></div>
+</div>
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="form-group row">
+	<label for="kodNoKP" class="col-sm-2 col-form-label">No KP</label>
+	<div class="col-sm-10"><div class="input-group mb-2">
+		<div class="input-group-prepend"><div class="input-group-text">@</div></div>
+		<input type="number" name="nokp" class="form-control" id="kodInputNoKP" placeholder="nokp">
+	</div></div>
+</div>
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="form-group row">
+	<label for="kodEmail" class="col-sm-2 col-form-label">Email</label>
+	<div class="col-sm-10"><div class="input-group mb-2">
+		<div class="input-group-prepend"><div class="input-group-text">@</div></div>
+		<input type="email" name="email" class="form-control" id="kodInputEmail" placeholder="email">
+	</div></div>
+</div>
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="form-group row">
+	<label for="kodJurusan" class="col-sm-2 col-form-label">Jurusan</label>
+	<div class="col-sm-10"><div class="input-group mb-2">
+		<div class="input-group-prepend"><div class="input-group-text">@</div></div>
+		<select name="jurusan" class="form-control" id="kodInputJurusan">
+		<option>Art</option>
+		<option>Akaun</option>
+		<option>Perniagaan</option>
+		<option>Komputer</option>
+		<option>Muzik</option>
+		</select>
+	</div></div>
+</div>
+<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////// -->
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="submit" class="btn btn-primary">Tambah Data</button>
+			</div>
+		</form>
+		</div>
+	</div>
+</div>
