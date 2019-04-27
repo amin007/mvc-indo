3d2dc43534f516128491ab4e4ccd9fb68f35b072
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 19:14:17 +0800

    susun pembolehubah

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,16 +4,13 @@
 $fail = 'Index';
 $folder = basename(__DIR__);//Returns The Current PHP Folder
 #-----------------------------------------------------------------------------------------------------------
-print <<<END
+?>
 <div class="container mt-5">
 <div class="jumbotron jumbotron-fluid">
 	<div class="container">
-		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
-		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
-		<p class="lead">Sini adalah kawasan larangan merokok.</p>
+		<h1 class="display-4">Views: Ini Folder <strong><?php echo $folder ?></strong></h1>
+		<p class="lead">Nama saya <?php echo $data['nama'] ?> </p>
 		<a class="btn btn-primary btn-lg" href="#" role="button">Kembali</a>
 	</div>
 </div>
 </div><!-- class="container" -->
-END;
-#-----------------------------------------------------------------------------------------------------------
