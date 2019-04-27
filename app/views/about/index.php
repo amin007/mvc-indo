b91053bf775e45639a0e33ff32b9191a111c0bcc
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 16:50:22 +0800

    masukkan tatasusunan dalam view

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,6 +5,9 @@ $fail = 'Index';
 $folder = basename(__DIR__);//Returns The Current PHP Folder
 #-----------------------------------------------------------------------------------------------------------
 diatas();
+/*echo '<p class="lead">Nama saya ' . $data['nama']
+. ', umur saya ' . $data['umur']
+. ', kerja saya ' . $data['kerja'] . '</p>//*/
 print <<<END
 <br><br>
 <div class="container mt-10">
@@ -12,11 +15,13 @@ print <<<END
 	<div class="container">
 		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
 		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
-		<p class="lead">Sini adalah kawasan larangan merokok.</p>
+		<p class="lead">Nama saya $data[nama],
+			umur saya $data[umur],
+			kerja saya $data[kerja]</p>
 		<a class="btn btn-primary btn-lg" href="../index.php" role="button">Kembali</a>
 	</div>
 </div>
 </div><!-- class="container" -->
 END;
-	dibawah();
+dibawah();
 #-----------------------------------------------------------------------------------------------------------
