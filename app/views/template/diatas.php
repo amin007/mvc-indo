968e8ea716def447438010257bc06e858aede937
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 19:21:17 +0800

    tambah pautan baru -> mahasiswa

--- app/views/template/diatas.php
+++ app/views/template/diatas.php
@@ -26,6 +26,7 @@
 	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 		<div class="navbar-nav">
 			<a class="nav-item nav-link active" href="<?php echo BASEURL?>">Home</a>
+			<a class="nav-item nav-link" href="<?php echo BASEURL?>mahasiswa">Mahasiswa</a>
 			<a class="nav-item nav-link" href="<?php echo BASEURL?>about">About</a>
 		</div>
 	</div>
