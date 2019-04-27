266f87e7570723db2b468dbbbea7782374544909
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 16:34:23 +0800

    tambah fail dalam folder view

---
+++ app/views/about/index.php
@@ -0,0 +1,22 @@
+<?php
+#-----------------------------------------------------------------------------------------------------------
+//$fail = basename($_SERVER['PHP_SELF']);//Returns The Current PHP File Name
+$fail = 'Index';
+$folder = basename(__DIR__);//Returns The Current PHP Folder
+#-----------------------------------------------------------------------------------------------------------
+diatas();
+print <<<END
+<br><br>
+<div class="container mt-10">
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
+		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
+		<p class="lead">Sini adalah kawasan larangan merokok.</p>
+		<a class="btn btn-primary btn-lg" href="../index.php" role="button">Kembali</a>
+	</div>
+</div>
+</div><!-- class="container" -->
+END;
+	dibawah();
+#-----------------------------------------------------------------------------------------------------------
