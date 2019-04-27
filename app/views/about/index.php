6799bbfcda59199eb380b659f52621c7215d9965
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-04-27 18:49:35 +0800

    tambah gambar

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,22 +4,13 @@
 $fail = 'Index';
 $folder = basename(__DIR__);//Returns The Current PHP Folder
 #-----------------------------------------------------------------------------------------------------------
-/*echo '<p class="lead">Nama saya ' . $data['nama']
-. ', umur saya ' . $data['umur']
-. ', kerja saya ' . $data['kerja'] . '</p>//*/
-print <<<END
-<br><br>
-<div class="container mt-10">
-<div class="jumbotron jumbotron-fluid">
-	<div class="container">
-		<h1 class="display-4">Views: Ini Folder <strong>$folder</strong></h1>
-		<h1 class="display-4">Views: Ini Fail <strong>$fail</strong></h1>
-		<p class="lead">Nama saya $data[nama],
-			umur saya $data[umur],
-			kerja saya $data[kerja]</p>
-		<a class="btn btn-primary btn-lg" href="../index.php" role="button">Kembali</a>
-	</div>
-</div>
+?>
+<div class="container">
+	<h1 class="mt-5"><?php echo $data['tajukAtas'] ?></h1>
+	<img src="<?php echo BASEURL ?>public/img/profile.jpg" alt="<?php echo $data['nama'] ?>"
+	width="10%" height="10%" class="rounded-circle shadow">
+	<p class="lead">Nama saya <?php echo $data['nama'] ?>,
+			umur saya <?php echo $data['umur'] ?>,
+			kerja saya <?php echo $data['kerja'] ?></p>
+	<a class="btn btn-primary btn-lg" href="<?php echo BASEURL ?>" role="button">Kembali</a>
 </div><!-- class="container" -->
-END;
-#-----------------------------------------------------------------------------------------------------------
