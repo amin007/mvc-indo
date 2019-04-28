5fa403f14a305d4a4dd5599ed9d6c9df221ba785
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-28 13:02:09 +0800

    tambah script js yang baru

--- app/views/template/dibawah.php
+++ app/views/template/dibawah.php
@@ -5,5 +5,6 @@
 <!-- Bootstrap Core JavaScript -->
 <!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="<?php echo BASEURL ?>public/js/script.js"></script>
 </body>
 </html>
