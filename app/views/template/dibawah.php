16fa05b6007bcf853d5df612c0c65ab70628dcee
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-28 14:44:01 +0800

    tambah jquery yang baru

--- app/views/template/dibawah.php
+++ app/views/template/dibawah.php
@@ -1,10 +1,14 @@
 <!-- jQuery -->
-<!-- script src="https://code.jquery.com/jquery-2.2.3.min.js"></script -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="<?php echo BASEURL ?>public/js/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <!-- Bootstrap Core JavaScript -->
-<!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="<?php echo BASEURL ?>public/js/script.js"></script>
 </body>
 </html>
+<?php
+/*
+<!-- script src="https://code.jquery.com/jquery-2.2.3.min.js"></script -->
+<!-- script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script -->
+<!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script -->
+*/
