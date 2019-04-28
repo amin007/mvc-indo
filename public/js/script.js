af29243c62c766b977841863c34e6f1def09a9be
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-28 14:16:48 +0800

    ada masalah nak baca

--- public/js/script.js
+++ public/js/script.js
@@ -9,15 +9,20 @@ $(function(){
 	$('.tampilModalUbah').on('click', function(){
 		$('#formModalLabel').html('Ubah Data Mahasiswa');
 		$('.modal-footer button[type=submit]').html('Ubah Data');
+		$('.modal-content').attr('action', 'http://jpmuar07-pc/projek-github/orang_punya/mvc-indo/v00/mahasiswa/ubah');
 
 		const id = $(this).data('id');//console.log(id);
 		$.ajax({
 			url: 'http://jpmuar07-pc/projek-github/orang_punya/mvc-indo/v00/mahasiswa/getubah',
-			data: { id:id },
+			data: {id : id},
 			method: 'POST',
-			//dataType: 'json',
-			success: function(data) {
-				console.log(data);
+			dataType: 'json',
+			success: function(data2) {
+				$('#kodInputNama').val(data2.nama);
+				$('#kodInputNoKP').val(data2.nokp);
+				$('#kodInputEmail').val(data2.email);
+				$('#kodInputJurusan').val(data2.jurusan);
+				$('#id').val(data2.id);
 			}
 		});
 	});
