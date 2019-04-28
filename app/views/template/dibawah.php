e7ce38ce6a4f5357e5272d2f3237c1a9eb530498
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-28 15:22:43 +0800

    masukkan beberapa versi jquery

--- app/views/template/dibawah.php
+++ app/views/template/dibawah.php
@@ -1,6 +1,5 @@
 <!-- jQuery -->
-<script src="<?php echo BASEURL ?>public/js/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="<?php echo BASEURL . 'public/js/jquery/jquery-3.4.0.min.js' ?>"></script>
 <!-- Bootstrap Core JavaScript -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="<?php echo BASEURL ?>public/js/script.js"></script>
@@ -10,5 +9,7 @@
 /*
 <!-- script src="https://code.jquery.com/jquery-2.2.3.min.js"></script -->
 <!-- script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script -->
+<!-- script src="https://code.jquery.com/jquery-3.4.0.min.js"></script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script -->
 */
