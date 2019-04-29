c37e6f04b1c2b3fa90a4483754ccb2e248956712
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-29 11:47:47 +0800

    alih kedudukan koding

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -5,8 +5,13 @@ $fail = 'Index';
 $folder = basename(__DIR__);//Returns The Current PHP Folder
 $p = BASEURL . 'mahasiswa';
 #-----------------------------------------------------------------------------------------------------------
-?><h3><?php echo $data['tajukAtas'] ?></h3>
+?>
 <div class="container mt-5">
+	<div class="row"><div class="col-lg-6">
+		<h3><?php echo $data['tajukAtas'] ?></h3>
+	</div><!-- / class="col-lg-6"> -->
+	</div><!-- / class="row" -->
+
 	<div class="card" style="width: 18rem;">
 		<div class="card-body">
 			<h5 class="card-title"><?php echo $data['mhs']['nama'] ?></h5>
