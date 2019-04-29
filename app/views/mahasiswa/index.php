b498195587dde840c80c79b0ecf64c8f7361b811
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-29 14:07:36 +0800

    ubah pautan

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -41,9 +41,9 @@ $folder = basename(__DIR__);//Returns The Current PHP Folder
 				echo $mhs['nama'] . "\n\t\t\t\t";
 			?><a href="<?php echo $p2 ?>" class="badge badge-danger float-right ml-1"
 			onclick="return confirm('yakin');">hapus</a>
-			<!-- a href="<?php echo $p3 ?>" class="badge badge-success float-right ml-1 tampilModalUbah"
-			data-toggle="modal" data-target="#formModal" data-id="<?php echo $mhs['id'] ?>">ubah</a -->
-			<a href="<?php echo $p4 ?>" class="badge badge-success float-right ml-1">ubah</a>
+			<a href="<?php echo $p3 ?>" class="badge badge-success float-right ml-1 tampilModalUbah"
+			data-toggle="modal" data-target="#formModal" data-id="<?php echo $mhs['id'] ?>">ubah</a>
+			<!-- a href="<?php echo $p4 ?>" class="badge badge-success float-right ml-1">ubah</a -->
 			<a href="<?php echo $p1 ?>" class="badge badge-primary float-right ml-1">detail</a>
 			</li><?php endforeach; echo "\n\t\t\t"; ?></ul><?php endif; echo "\n"; ?>
 		</div><!-- / class="col-6" -->
