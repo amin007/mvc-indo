2f8f55d3ef97893373031374b6bb481f5370064d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-05-13 15:50:47 +0800

    setkan pembolehubah utama

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -24,6 +24,15 @@ class Login extends Controller
 		//echo '<hr>Nama class :' . __METHOD__ . '<hr>';
 		$data['tajukAtas'] = 'Ruangtamu';
 		$data['bgcolor'] = 'style="background-color:#d3d3d3;"';
+		//////////////////////////////////////////////////////////////////////////////////
+			$data['username'] = 'ayam';
+			$data['password'] = md5('ayam');
+			$data['firstname'] = 'ayam';
+			$data['middlename'] = 'bin';
+			$data['lastname'] = 'jantan';
+			$data['admin_id'] = 1;
+
+		//////////////////////////////////////////////////////////////////////////////////
 		$this->view('template/ruangtamu/diatas', $data);
 		$this->view('template/ruangtamu/dimenu', $data);
 		$this->view('ruangtamu/index',  $data);
