866599be131d04771c2ce615925f2c7626599e10
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-05-13 14:24:32 +0800

    tukar tajukAtas dan bgcolor

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -11,7 +11,8 @@ class Login extends Controller
 #------------------------------------------------------------------------------------------
 	public function index($nama = 'Rahsia', $kerja = 'Tanam Anggur', $umur = 25)
 	{
-		$data['tajukAtas'] = 'Saya';
+		$data['tajukAtas'] = 'Login';
+		$data['bgcolor'] = 'style="background-color:#d3d3d3;"';
 		$this->view('template/diatas', $data);
 		$this->view('template/khas001/dimenu', $data);
 		$this->view('login/fail_login',  $data);
