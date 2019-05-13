5da706838e7402e1e67d229fc5129ac7a56e9a41
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-05-13 13:49:52 +0800

    tambah gambar logo

--- app/views/template/khas001/dimenu.php
+++ app/views/template/khas001/dimenu.php
@@ -1,8 +1,8 @@
-		<nav class = "navbar navbar-default navbar-fixed-top">
-			<div class = "container-fluid">
-				<div class = "navbar-header">
-					<img src = "images/logo.png" width = "50px" height = "50px" />
-					<h4 class = "navbar-text navbar-right">Borrowing Item Equipment System (BITES)</h4>
-				</div>
-			</div>
-		</nav>
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<img src="<?php echo BASEURL?>public/img/logo/logo.png" width="50px" height="50px" />
+			<h4 class="navbar-text navbar-right">Borrowing Item Equipment System (BITES)</h4>
+		</div>
+	</div>
+</nav>
