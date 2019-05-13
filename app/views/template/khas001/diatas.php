11a9fe46abbf00078b462fecab76cae51fabd1f9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-05-13 13:39:40 +0800

    tambah template khas

---
+++ app/views/template/khas001/diatas.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang = "eng">
+<head>
+	<title>Borrowing Item Equipment System (BITES)</title>
+	<meta charset = "utf-8" />
+	<meta name = "viewport" content = "width=device-width, initial-scale=1" />
+	<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+</head>
+<body style = "background-color:#d3d3d3;">
---
+++ app/views/template/khas001/dibawah.php
@@ -0,0 +1,10 @@
+	<nav class = "navbar navbar-default navbar-fixed-bottom">
+		<div class = "container-fluid">
+			<label class = "navbar-text pull-right">Borrowing Item Equipment System (BITES) &copy; All rights reserved 2016</label>
+		</div>
+	</nav>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>
+<!-- script src = "js/login.js"></script -->
+</html>
