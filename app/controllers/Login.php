0555842473058b5a89b8102170b818797e61881d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-05-13 17:55:07 +0800

    tambah fungsi items

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -72,6 +72,30 @@ class Login extends Controller
 		$this->view('template/ruangtamu/dibawah');
 	}
 #------------------------------------------------------------------------------------------
+	public function items($fail)
+	{
+		//echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+		$data['tajukAtas'] = 'Items';
+		$data['bgcolor'] = 'style="background-color:#d3d3d3;"';
+		$data['fail'] = 'Items / ' . $fail;
+		//////////////////////////////////////////////////////////////////////////////////
+			$data['select'] = '<input type="hidden" name="book_id[]" value = "1">'
+			. '<center><input type="checkbox" name="selector[]" value = "1">'
+			. '</center>';
+			$data['book title'] = 'ayam goreng';
+			$data['book category'] = md5('ayam');
+			$data['book author'] = 'ayam kfc';
+			$data['sate published'] = 'bin';
+			$data['quantity'] = '25';
+			$data['left'] = '25';
+			$data['admin_id'] = 1;
+		//////////////////////////////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////////////////
+		$this->view('template/ruangtamu/diatas', $data);
+		$this->view('template/ruangtamu/dimenu', $data);
+		$this->view('ruangtamu/jadual',  $data);
+		$this->view('template/ruangtamu/dibawah');
+	}
 #------------------------------------------------------------------------------------------
 #==========================================================================================
 }
